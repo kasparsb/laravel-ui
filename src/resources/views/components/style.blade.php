dac39cf6c8e47fc9839de0185c474bd35cb4c247
root <root@note2.my.domain>
2024-03-15 14:40:03 +0000

    Font import izvakts no css, ielikt style head

--- src/resources/views/components/style.blade.php
+++ src/resources/views/components/style.blade.php
@@ -1,1 +1,5 @@
-<link href="{{ asset('/vendor/ui/dist/app.min-0.0.2.css') }}" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400..600&display=swap" rel="stylesheet">
+
+<link href="{{ asset('/vendor/ui/dist/app.min-0.0.4.css') }}" rel="stylesheet">
