b351c02a115eee6cdacf0bac00c74c5ca1b7a7c8
root <root@note2.my.domain>
2024-04-24 17:20:11 +0000

    Salabots, lai table nemekle elementu, kura nav

--- src/resources/js/Table.js
+++ src/resources/js/Table.js
@@ -4,22 +4,25 @@ export default {
     init() {
 
         on('focusout', 'input', (ev, el) => {
-            let data = getFormData(parent(el, 'tr'));
-            let link = '';
-            if (data.id) {
-                link = parent(el, '.table').dataset.linkUpdate;
+            let tr = parent(el, 'tr');
+            if (tr) {
+                let data = getFormData(parent(el, 'tr'));
+                let link = '';
+                if (data.id) {
+                    link = parent(el, '.table').dataset.linkUpdate;
 
-                /**
-                 * TODO vajag kaut kā dabūt link, lai te nekas nav jārepleico
-                 */
-                link = link.replace('#id#', data.id);
-            }
-            else {
-                link = parent(el, '.table').dataset.linkCreate;
-            }
+                    /**
+                     * TODO vajag kaut kā dabūt link, lai te nekas nav jārepleico
+                     */
+                    link = link.replace('#id#', data.id);
+                }
+                else {
+                    link = parent(el, '.table').dataset.linkCreate;
+                }
 
-            post(link, data)
-                .then(r => console.log(r));
+                post(link, data)
+                    .then(r => console.log(r));
+            }
         })
 
     }
