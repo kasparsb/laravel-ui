f07d47bdbd30b935834464fecdd4b1e14d7f0568
root <root@note2.my.domain>
2024-05-19 08:09:04 +0000

    Lapu sadalijums ar delimiter uzrakstits logiskaks un vienkarsaks

--- src/View/Components/Pagination.php
+++ src/View/Components/Pagination.php
@@ -22,7 +22,7 @@ class Pagination extends Component
     public $elements = [];
 
     public function __construct(
-        public ?Paginator $paginator = null, // Laravel paginator instance
+        public $paginator = null, // Laravel paginator instance
 
         // Ja nav padots paginator, tad manuāli izveidot pages count
         public $itemsPerPage = null,
@@ -31,21 +31,26 @@ class Pagination extends Component
 
         // Cik pages rādīt ap currentPage
         public $onEachSide = 3,
+        // Cik lapas rādīt no sākuma un beigām
+        public $onEdges = 2,
 
         // Link template #page# tas ir page number, kas tiks replaced
         public $link = '',
     )
     {
-        if ($paginator) {
-            $this->hasPages = $paginator->hasPages();
-            $this->onFirstPage = $paginator->onFirstPage();
-            $this->hasMorePages = $paginator->hasMorePages();
-            $this->currentPage = $paginator->currentPage();
-            $this->itemPerPage = $paginator->perPage();
-            $this->itemsCount = $paginator->total();
+        /**
+         * Sākuma vērtības atkarībā no tā vai ir pieejams paginator vai nav
+         */
+        if ($this->paginator) {
+            $this->hasPages = $this->paginator->hasPages();
+            $this->onFirstPage = $this->paginator->onFirstPage();
+            $this->hasMorePages = $this->paginator->hasMorePages();
+            $this->currentPage = $this->paginator->currentPage();
+            $this->itemsPerPage = $this->paginator->perPage();
+            $this->itemsCount = $this->paginator->total();
 
-            $this->previousPageUrl = $paginator->previousPageUrl();
-            $this->nextPageUrl = $paginator->nextPageUrl();
+            $this->previousPageUrl = $this->paginator->previousPageUrl();
+            $this->nextPageUrl = $this->paginator->nextPageUrl();
 
             $this->pagesCount = ceil($this->itemsCount / $this->itemsPerPage);
         }
@@ -59,57 +64,87 @@ class Pagination extends Component
 
             $this->hasPages = $this->itemsCount > $this->itemsPerPage;
             $this->onFirstPage = $this->currentPage === 1;
-            $this->hasMorePages = $pagesCount > $this->currentPage;
+            $this->hasMorePages = $this->pagesCount > $this->currentPage;
 
-            $this->previousPageUrl = $this->pageLinkFromTemplate($this->currentPage - 1);;
-            $this->nextPageUrl = $this->pageLinkFromTemplate($this->currentPage + 1);
+            $this->previousPageUrl = $this->pageLink($this->currentPage - 1);
+            $this->nextPageUrl = $this->pageLink($this->currentPage + 1);
         }
 
 
 
-        $pages = [];
-        $minPage = null;
-        $maxPage = null;
-        for ($c = max(1, $this->currentPage - $this->onEachSide); $c < min($this->currentPage + $this->onEachSide + 1, $this->pagesCount); $c++) {
-            if (is_null($minPage)) {
-                $minPage = $c;
+
+
+
+        /**
+         * Saliekam redzamās lapas
+         */
+
+
+
+        // Sākuma lapas
+        for ($pageNumber = 1; $pageNumber <= $this->onEdges; $pageNumber++) {
+            $pages[$pageNumber] = $this->pageLink($pageNumber);
+        }
+
+        // Lapas ap current page
+        for (
+            $pageNumber = max(1, $this->currentPage - $this->onEachSide); // lai neierauj mīnusā
+            $pageNumber <= min($this->currentPage + $this->onEachSide, $this->pagesCount); // lai nepārkāpj pāri pageCount
+            $pageNumber++
+        ) {
+            $pages[$pageNumber] = $this->pageLink($pageNumber);
+        }
+
+        // Beigu lapas
+        for ($pageNumber = $this->pagesCount - $this->onEdges + 1; $pageNumber <= $this->pagesCount; $pageNumber++) {
+            $pages[$pageNumber] = $this->pageLink($pageNumber);
+        }
+
+
+        /**
+         * Saliekm atdalītājus starp lapām, kuras nav secīgas
+         * 1 2 3 4 5   10 11 12
+         *          ...
+         * 1 2   5 6 7   10 11 12
+         *    ...     ...
+         */
+        $prevPageNumber = null;
+        $portion = [];
+        foreach ($pages as $pageNumber => $link) {
+            // Starpā ir vairāk kā viena lapa
+            if ($prevPageNumber && $pageNumber - $prevPageNumber > 2) {
+                $this->elements[] = $portion;
+                $this->elements[] = '...';
+                $portion = [];
             }
-            $maxPage = $c;
+            // starpā ir viena lapa, tāpēc liekam to lapu nevis delimiter
+            else if ($prevPageNumber && $pageNumber - $prevPageNumber > 1) {
+                $portion[$pageNumber - 1] = $this->pageLink($pageNumber - 1);
+                $this->elements[] = $portion;
+                $portion = [];
+            }
 
-            $pages[$c] = $this->paginator ? $$this->paginator->link($c) : $this->pageLinkFromTemplate($c);
+            $portion[$pageNumber] = $link;
+
+            $prevPageNumber = $pageNumber;
         }
-
-        if ($minPage > 2) {
-            $this->elements[] = '...';
+        if ($portion) {
+            $this->elements[] = $portion;
         }
+    }
 
-        $this->elements[] = $pages;
-
-        if ($maxPage < $pagesCount - 2) {
-            $this->elements[] = '...';
+    private function pageLink($pageNumber) {
+        if ($this->paginator) {
+            return $this->paginator->url($pageNumber);
+        }
+        else {
+            $r = str_replace('#page#', $pageNumber, $this->link);
+            return str_replace('%23page%23', $pageNumber, $r);
         }
     }
 
     public function render(): View|Closure|string
     {
         return view('ui::components.pagination');
-    }
-
-    private function pageLinkFromTemplate($pageNumber) {
-        $link = $this->link;
-        $link = str_replace('#page#', $pageNumber, $link);
-        $link = str_replace('%23page%23', $pageNumber, $link);
-
-        return $link;
-    }
-
-    private function clampPage($pageNumber) {
-        if ($pageNumber < 1) {
-            return 1;
-        }
-        if ($pageNumber > $this->pagesCount) {
-            return $this->pagesCount;
-        }
-        return $pageNumber;
     }
 }
