58d62f93e8e9f21d61f745bb2cc22e20d52d48ed
root <root@note2.my.domain>
2024-06-02 11:04:56 +0000

    Field text un textarea pielikts laravel error messages bag, no kura nolasa field kludu pec varad

--- src/View/Components/FieldText.php
+++ src/View/Components/FieldText.php
@@ -16,12 +16,21 @@ class FieldText extends Component
         public $placeholder='',
         public $model=null,
         public $disabled=false,
+
         public $error='',
         public $hasError=false,
+        public $errors=false,
     )
     {
         if ($this->model) {
             $this->value = $this->model->{$this->name};
+        }
+
+        if ($this->errors) {
+            if ($this->errors->has($this->name)) {
+                $this->error = $this->errors->first($this->name);
+                $this->hasError = true;
+            }
         }
     }
 
