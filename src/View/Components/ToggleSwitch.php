a4b15cc9f7a43d4992ce026c012bbd8283b6607e
root <root@note2.my.domain>
2024-06-04 20:00:18 +0000

    Toggle switch description, error state

--- src/View/Components/ToggleSwitch.php
+++ src/View/Components/ToggleSwitch.php
@@ -5,9 +5,17 @@ namespace Kasparsb\Ui\View\Components;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
+use Illuminate\Support\ViewErrorBag;
+use Illuminate\Http\Request;
+use Kasparsb\Ui\ComponentWithError;
+use Kasparsb\Ui\ComponentWithRequestOldValue;
 
 class ToggleSwitch extends Component
 {
+
+    use ComponentWithError;
+    use ComponentWithRequestOldValue;
+
     public function __construct(
         public $name='',
         public $label='',
@@ -15,11 +23,24 @@ class ToggleSwitch extends Component
         public $checked=false,
         public $description='',
         public $model=null,
+        public $disabled=false,
+
+        public $errorMessage='',
+        public $hasError=false,
+        // Laravel errors
+        public ?ViewErrorBag $errors=null,
+
+        // No šī ņems vērtību, kura tika iepostēta
+        public ?Request $request=null,
     )
     {
-        if ($this->model) {
-            $this->checked = $this->model->{$this->name} ? true : false;
+        if (!$this->setOldValueCheckbox()) {
+            if ($this->model) {
+                $this->checked = $this->model->{$this->name} ? true : false;
+            }
         }
+
+        $this->setError();
     }
 
     public function render(): View|Closure|string
--- src/resources/views/components/toggle-switch.blade.php
+++ src/resources/views/components/toggle-switch.blade.php
@@ -1,20 +1,23 @@
-<label class="toggle-switch">
-
-    @if ($labelPosition == 'left')
-        @if ($label)
-        <span>{{ $label }}</span>
+<div
+    {{ $attributes->class(['toggle-switch']) }}
+    data-state="{{ $hasError ? 'error' : '' }}"
+    >
+    <label>
+        @if ($labelPosition == 'left')
+            @if ($label)
+            <span>{{ $label }}</span>
+            @endif
         @endif
-    @endif
+        <span>
+            <input type="checkbox" name="{{ $name }}" @checked($checked) value="1" />
+        </span>
 
-
-    <span>
-        <input type="checkbox" name="{{ $name }}" @checked($checked) value="1" />
-    </span>
-
-    @if ($labelPosition == 'right')
-        @if ($label)
-        <span>{{ $label }}</span>
+        @if ($labelPosition == 'right')
+            @if ($label)
+            <span>{{ $label }}</span>
+            @endif
         @endif
-    @endif
-
-</label>
+    </label>
+    <p data-role="description">{{ $description }}</p>
+    <p data-role="error">{{ $errorMessage }}</p>
+</div>
