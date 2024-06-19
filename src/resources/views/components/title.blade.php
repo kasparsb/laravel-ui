b73e4e24a61448c90ed73c08f84e08f3ba63d2d6
root <root@note2.my.domain>
2024-06-19 12:08:25 +0000

    Title vai ir text size klases noteiksana parlikta uz Helper

--- src/resources/views/components/title.blade.php
+++ src/resources/views/components/title.blade.php
@@ -1,14 +1,6 @@
-@php
-// Meklējam vai ir pielikta custom text size klase
-$textSizeClassApplied = collect(explode(' ', $attributes->get('class')))->first(function($className){
-    // Izvācam . jo ir klases 3.5 4.5 utt
-    $className = str_replace('.', '', $className);
-    preg_match('/^t-([0-9]*)$/', $className, $matches);
-    return count($matches) > 0;
-});
-$defaultClasses = ['title'];
-if (!$textSizeClassApplied) {
-    $defaultClasses[] = 't-6';
-}
-@endphp
-<h2 {{ $attributes->class($defaultClasses) }}>{{ $slot }}</h2>
+@inject('helpers', 'Kasparsb\Ui\Helpers')
+
+<h2 {{ $attributes->class([
+    'title' => true,
+    't-6' => !$helpers->hasAnyTextSizeClass($attributes->get('class'))
+]) }}>{{ $slot }}</h2>
