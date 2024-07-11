d3d2a8839810d53157d6ecb097d06b1f9a112717
root <root@note2.my.domain>
2024-07-11 07:17:23 +0000

    Dropdown menu border color ar css klasi

--- src/resources/views/components/dropdown-menu.blade.php
+++ src/resources/views/components/dropdown-menu.blade.php
@@ -1,9 +1,11 @@
+@inject('helpers', 'Kasparsb\Ui\Helpers')
 <div
     {{ $attributes->class([
         'dropdown-menu',
         'static' => $isStatic,
         'floating' => !$isStatic,
         'hidden' => !$isStatic,
+        'b-c-200' => !$helpers->hasAnyBorderColorClass($attributes->get('class'))
     ]) }}
     data-dropdown-menu-name="{{ $name }}"
     data-side="{{ $side }}"
