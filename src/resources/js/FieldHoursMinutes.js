facfb43a6323c59103250313179deb596abc05f7
root <root@note2.my.domain>
2024-07-22 17:16:56 +0000

    Field hours minutes pielikts setValue

--- src/resources/js/FieldHoursMinutes.js
+++ src/resources/js/FieldHoursMinutes.js
@@ -1,11 +1,29 @@
 import {q, qa, on, change, parent} from 'dom-helpers'
 
-function updateValue(fieldEl) {
+function updateValue(fieldEl, value) {
+    if (typeof value == 'undefined') {
+        let incrementFields = [...qa(fieldEl, '.field-increment input')];
+        value = incrementFields.at(0).value
+            + ':'
+            + incrementFields.at(1).value;
+    }
+
+    q(fieldEl, 'input[type=hidden]').value = value
+}
+
+function displayValue(fieldEl) {
+    let value = q(fieldEl, 'input[type=hidden]').value;
+    if (!value) {
+        value = '';
+    }
+    let p = value.split(':');
     let incrementFields = [...qa(fieldEl, '.field-increment input')];
-    q(fieldEl, 'input[type=hidden]').value =
-        incrementFields.at(0).value
-        + ':'
-        + incrementFields.at(1).value;
+    if (p.length > 0) {
+        incrementFields.at(0).value = p[0];
+    }
+    if (p.length > 1) {
+        incrementFields.at(1).value = p[1];
+    }
 }
 
 export default {
@@ -21,5 +39,11 @@ export default {
                 updateValue(parent(el, '.field-hours-minutes'));
             }, 200)
         })
+    },
+
+    setValue(fieldElOrInputEl, value) {
+        let fieldEl = parent(fieldElOrInputEl, '.field-hours-minutes');
+        updateValue(fieldEl, value);
+        displayValue(fieldEl);
     }
 }
