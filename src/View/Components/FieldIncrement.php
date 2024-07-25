2ed1b06b6e451908efce1fe0c10f7a26736b3932
root <root@note2.my.domain>
2024-07-25 06:49:52 +0000

    Field increment menu position at selector

--- src/View/Components/FieldIncrement.php
+++ src/View/Components/FieldIncrement.php
@@ -37,6 +37,7 @@ class FieldIncrement extends Component
         public $menuShow = '',
         public $menuHide = null, // null nozīmē, ka nav uzsetots. Var padot arī empty string vai boolean
         public $menuResetForm = false,
+        public $menuPositionAt = null,
 
         public $min = 0,
         public $max = null,
