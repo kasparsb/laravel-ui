cd1cd2ac13e13708f4177b2791eb17bf61a14ed5
root <root@note2.my.domain>
2024-08-21 11:30:14 +0000

    Title font weight klase tiek likta no php, lai var override

--- src/resources/views/components/title.blade.php
+++ src/resources/views/components/title.blade.php
@@ -2,5 +2,6 @@
 
 <h2 {{ $attributes->class([
     'title' => true,
-    't-6' => !$helpers->hasAnyTextSizeClass($attributes->get('class'))
+    't-6' => !$helpers->hasAnyTextSizeClass($attributes->get('class')),
+    'fw-b' => !$helpers->hasAnyFontWeightClass($attributes->get('class'))
 ]) }}>{{ $slot }}</h2>
