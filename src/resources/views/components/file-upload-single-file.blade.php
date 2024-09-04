dd0451ff469ea2b74db59c3a4041ca7dabb89b7e
root <root@note2.my.domain>
2024-09-04 20:30:41 +0000

    Single file partaisits progress bar novietojumus

--- src/resources/views/components/file-upload-single-file.blade.php
+++ src/resources/views/components/file-upload-single-file.blade.php
@@ -46,54 +46,59 @@
     </x-ui::aspect-ratio>
 
     <div data-r="controls">
-        <div class="icon">
-            <svg data-type="document" width="24" height="24" viewBox="0 0 24 24">
-                <use xlink:href="#ui-icon-document"></use>
-            </svg>
-            <svg data-type="image" width="24" height="24" viewBox="0 0 24 24">
-                <use xlink:href="#ui-icon-image"></use>
-            </svg>
-            <svg data-type="video" width="24" height="24" viewBox="0 0 24 24">
-                <use xlink:href="#ui-icon-video"></use>
-            </svg>
-            <svg data-type="audio" width="24" height="24" viewBox="0 0 24 24">
-                <use xlink:href="#ui-icon-audio"></use>
-            </svg>
-            <svg data-type="archive" width="24" height="24" viewBox="0 0 24 24">
-                <use xlink:href="#ui-icon-archive"></use>
-            </svg>
-        </div>
-        <div>
-            <x-ui::title class="t-3.5" data-r="fileName">
-                @if ($fileName) {{ $fileName }} @else &nbsp; @endif
-            </x-ui::title>
-            <x-ui::title-description data-r="fileDescription">
-                @if ($fileDescription) {{ $fileDescription }} @else &nbsp; @endif
-            </x-ui::title-description>
-
-            <x-ui::progress-bar progress="{{ $progress }}" data-r="progressBar" />
-
-            <div class="failed-message" data-r="failedMessage">
-                @if ($error) {{ $error }} @else &nbsp; @endif
-            </div>
-        </div>
-        <x-ui::h-stack class="gap-0">
-
-            <x-ui::button-link
-                as="link"
-                data-r="button-download"
-                :link="$downloadLink"
-                >Download</x-ui::button-link>
-
-            <x-ui::button-outline
-                class="icon"
-                data-r="button-remove">
-                <svg width="24" height="24" viewBox="0 0 24 24">
-                    <use xlink:href="#ui-icon-x"></use>
+        <x-ui::h-stack class="v-top">
+            <div class="file-type-icon">
+                <svg data-type="document" width="24" height="24" viewBox="0 0 24 24">
+                    <use xlink:href="#ui-icon-document"></use>
                 </svg>
-            </x-ui::button-outline>
+                <svg data-type="image" width="24" height="24" viewBox="0 0 24 24">
+                    <use xlink:href="#ui-icon-image"></use>
+                </svg>
+                <svg data-type="video" width="24" height="24" viewBox="0 0 24 24">
+                    <use xlink:href="#ui-icon-video"></use>
+                </svg>
+                <svg data-type="audio" width="24" height="24" viewBox="0 0 24 24">
+                    <use xlink:href="#ui-icon-audio"></use>
+                </svg>
+                <svg data-type="archive" width="24" height="24" viewBox="0 0 24 24">
+                    <use xlink:href="#ui-icon-archive"></use>
+                </svg>
+            </div>
+
+            <x-ui::v-stack class="gap-1 h-stretch">
+                <x-ui::h-stack>
+                    <x-ui::v-stack class="gap-0 h-stretch">
+                        <x-ui::title class="t-3.5" data-r="fileName">
+                            @if ($fileName) {{ $fileName }} @else &nbsp; @endif
+                        </x-ui::title>
+                        <x-ui::title-description data-r="fileDescription">
+                            @if ($fileDescription) {{ $fileDescription }} @else &nbsp; @endif
+                        </x-ui::title-description>
+                        <div class="failed-message" data-r="failedMessage">
+                            @if ($error) {{ $error }} @else &nbsp; @endif
+                        </div>
+                    </x-ui::v-stack>
+                    <x-ui::h-stack class="gap-0">
+                        <x-ui::button-link
+                            as="link"
+                            data-r="button-download"
+                            :link="$downloadLink"
+                            >Download</x-ui::button-link>
+                        <x-ui::button-outline
+                            class="icon"
+                            data-r="button-remove">
+                            <svg width="24" height="24" viewBox="0 0 24 24">
+                                <use xlink:href="#ui-icon-x"></use>
+                            </svg>
+                        </x-ui::button-outline>
+                    </x-ui::h-stack>
+                </x-ui::h-stack>
+
+                <x-ui::progress-bar progress="{{ $progress }}" data-r="progressBar" />
+            </x-ui::v-stack>
 
         </x-ui::h-stack>
+
     </div>
 
     <input type="hidden" name="{{ $name }}" value="{{ $value }}" data-r="input" />
