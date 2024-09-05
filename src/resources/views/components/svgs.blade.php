dfeec3359865b7cfb4b5cfd3c5fd7b85e30407aa
root <root@note2.my.domain>
2024-09-05 09:51:20 +0000

    Download svg ikona

--- src/resources/views/components/svgs.blade.php
+++ src/resources/views/components/svgs.blade.php
@@ -122,4 +122,11 @@
         <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
         <path d="M21 21l-6 -6" />
     </symbol>
+
+    <symbol id="ui-icon-download" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
+        <path d="M7 11l5 5l5 -5" />
+        <path d="M12 4l0 12" />
+    </symbol>
 </svg>
