5cf0834d2e110b33055248c68a4642d65def3d4f
root <root@note2.my.domain>
2024-10-08 18:23:07 +0000

    Details pielikta papildus parbauda, ja ir padots object

--- src/resources/views/components/details.blade.php
+++ src/resources/views/components/details.blade.php
@@ -1,7 +1,7 @@
 <dl {{ $attributes->class([
     'details-list' => true,
 ]) }}>
-    @if (is_iterable($details))
+    @if (is_iterable($details) || is_object($details))
         @foreach ($details as $term => $description)
         <x-ui::detail :term="$term" :description="$description" />
         @endforeach
