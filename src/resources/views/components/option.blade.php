dd5197a7f09134890d0deefa8448952db4f27675
root <root@note2.my.domain>
2024-10-11 12:52:03 +0000

    Options component pielikta pazimi, lai aizver tikai savu dropdown menu

--- src/resources/views/components/option.blade.php
+++ src/resources/views/components/option.blade.php
@@ -1,5 +1,8 @@
 <div
-    {{ $attributes->class([ 'menu-item', ]) }}
+    {{ $attributes->class([
+        'menu-item',
+    ]) }}
+    data-dropdown-menu-close-stack="false"
     data-options-list-option
     data-value="{{ $value }}"
     {{ $checked ? 'data-checked' : '' }}
