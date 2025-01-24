4c772eb11f60d1c6431650f83f3c4170846a2f9e
root <root@note2.my.domain>
2025-01-24 12:03:18 +0000

    Nav menu uzliek default layout un menu item visual

--- src/resources/views/components/nav-menu.blade.php
+++ src/resources/views/components/nav-menu.blade.php
@@ -1,5 +1,9 @@
+@inject('helpers', 'Kasparsb\Ui\Helpers')
+
 <nav {{ $attributes->class([
     'nav-menu',
+    'nav-menu-vertical' => !$helpers->hasAnyNavMenuLayoutClass($attributes->get('class')),
+    'menu-item-background' => !$helpers->hasAnyMenuItemVisualClass($attributes->get('class')),
 ]) }}>
     {{ $slot }}
 </nav>
