e54400b4e121b2446258603d4987242df6f8267a
root <root@note2.my.domain>
2025-02-07 18:55:43 +0000

    Container loading style attribute

--- src/resources/views/components/container.blade.php
+++ src/resources/views/components/container.blade.php
@@ -1,7 +1,10 @@
 <div
     {{ $attributes->class([
-    'container' => true,
+        'container' => true,
     ]) }}
+    @if ($loadingStyle)
+    data-loading-style="{{ $loadingStyle }}"
+    @endif
     @if ($loading)
     data-loading="{{ $loading === true ? 'loading' : $loading }}"
     @endif
