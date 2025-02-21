e6d031966cf00943c339abd6e88f00d5be19db8a
root <root@note2.my.domain>
2025-02-21 17:29:36 +0000

    Button copy can copy elements innerHTML

--- src/resources/js/ButtonCopy.js
+++ src/resources/js/ButtonCopy.js
@@ -1,24 +1,41 @@
 import { clickp, q, qr } from 'dom-helpers';
 
+function isInputField(el) {
+    switch (el.tagName) {
+        case 'INPUT':
+        case 'TEXTAREA':
+            return true;
+    }
+    return false;
+}
+
 function copyToClipboard(el) {
     if (!el) {
         return
     }
-
-    // Select the text field
-    el.select();
-    el.setSelectionRange(0, 99999); // For mobile devices
 
     if (!('clipboard' in navigator)) {
         console.error('Could not copy text: clipboard.navigator not available. Probaly your site is not served over https');
         return
     }
 
+    let elText = '';
+    if (isInputField(el)) {
+        // Select the text field
+        el.select();
+        el.setSelectionRange(0, 99999); // For mobile devices
+
+        elText = el.value;
+    }
+    else {
+        elText = el.innerHTML;
+    }
+
     // Copy the text inside the text field
-    navigator.clipboard.writeText(el.value).then(() => {
-    }).catch(err => {
-        console.error('Could not copy text: ', err);
-    });
+    navigator.clipboard.writeText(elText)
+        .catch(err => {
+            console.error('Could not copy text: ', err);
+        });
 }
 
 export default {
