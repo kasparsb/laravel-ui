15e2bed0e5218402c74322289f9d9c5a5d49cdf9
root <root@note2.my.domain>
2025-03-18 20:15:41 +0000

    NavMenu default selected value uzlikts null

--- src/View/Components/NavMenu.php
+++ src/View/Components/NavMenu.php
@@ -10,7 +10,7 @@ class NavMenu extends Component
 {
     public function __construct(
         // Kurš menu item ir selected
-        public $selected='',
+        public $selected=null,
     )
     {
     }
